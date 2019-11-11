ae7847eb722cbb77894c5b65c1af27d37ed5025b
Brendan Cannell <brendan@cannells.com>
2019-11-11 14:04:43 -0500

    Updates

---
+++ precache-manifest.9ac73976537f77d130e6797fa0166791.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89a278a6c98af9c8c4675e4d697247be",
+    "url": "/life/index.html"
+  },
+  {
+    "revision": "16e05f3734611a8be90d",
+    "url": "/life/static/css/main.924960f1.chunk.css"
+  },
+  {
+    "revision": "70662fa80718858fea07",
+    "url": "/life/static/js/2.d2d715f6.chunk.js"
+  },
+  {
+    "revision": "16e05f3734611a8be90d",
+    "url": "/life/static/js/main.d1252352.chunk.js"
+  },
+  {
+    "revision": "717dd7cd4ece77290c30",
+    "url": "/life/static/js/runtime~main.3a234e95.js"
+  }
+]);
