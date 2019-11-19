6ae3731a016cdbdc6118e659d1569b2695f94d0d
Brendan Cannell <brendan@cannells.com>
2019-11-19 14:30:59 -0500

    Updates

---
+++ precache-manifest.46626d30492909475ac30539eea62ed0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87fd252dea35ade44be450a616947896",
+    "url": "/life/index.html"
+  },
+  {
+    "revision": "b257658e49b36c772430",
+    "url": "/life/static/css/main.561dcf67.chunk.css"
+  },
+  {
+    "revision": "d38d0dcaee3090b94028",
+    "url": "/life/static/js/2.260373f2.chunk.js"
+  },
+  {
+    "revision": "b257658e49b36c772430",
+    "url": "/life/static/js/main.20cd2609.chunk.js"
+  },
+  {
+    "revision": "717dd7cd4ece77290c30",
+    "url": "/life/static/js/runtime~main.3a234e95.js"
+  }
+]);
