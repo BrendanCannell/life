be15fa539b2a31927c972e66e30c2c92576915da
Brendan Cannell <brendan@cannells.com>
2019-11-27 18:50:19 -0500

    Fixed failure to clear grid lines when scale drops below threshold

--- src/components/InteractiveViewer/render-grid-lines.js
+++ src/components/InteractiveViewer/render-grid-lines.js
@@ -1,33 +1,35 @@
 import {GridToImageX, GridToImageY} from "./coordinates"
 
-export default function RenderGridLines({colors, context: ctx, viewport}) {
-  if (viewport.scale < FADE_MIN) return
-  let {top, bottom, left, right, scale} = viewport
-  let color = Color(scale, colors)
+export default function RenderGridLines({colors, context: ctx, viewport: v}) {
   ctx.save()
+  // Clear canvas
   ctx.fillStyle = `rgba(${colors.dead.join()})`
   ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height)
-  ctx.fillStyle = `rgba(${color.join()})`
-  for (let xGrid = ceil(left); xGrid < right; xGrid++) {
-    let xImage = GridToImageX(xGrid, viewport) | 0
+  // Abort if `scale` is too small for grid lines
+  if (v.scale < MIN_SCALE) return ctx.restore()
+  // Vertical grid lines
+  ctx.fillStyle = GridLinesColor(v.scale, colors)
+  for (let xGrid = ceil(v.left); xGrid < v.right; xGrid++) {
+    let xImage = GridToImageX(xGrid, v) | 0
     ctx.fillRect(xImage, 0, 1, ctx.canvas.height)
   }
-  for (let yGrid = ceil(top); yGrid < bottom; yGrid++) {
-    let yImage = GridToImageY(yGrid, viewport) | 0
+  // Horizontal grid lines
+  for (let yGrid = ceil(v.top); yGrid < v.bottom; yGrid++) {
+    let yImage = GridToImageY(yGrid, v) | 0
     ctx.fillRect(0, yImage, ctx.canvas.width, 1)
   }
   ctx.restore()
 }
 
-let FADE_MIN = 5
+let MIN_SCALE = 5
 let FADE_EXPONENT = 0.15
 
-function Color(scale, colors) {
+// Grid lines fade into the background color as `scale` decreases
+function GridLinesColor(scale, colors) {
   let {lines, dead} = colors
-  scale *= window.devicePixelRatio || 1
-  let fade = pow(FADE_MIN / scale, FADE_EXPONENT)
-  let rgb = [...Array(3)].map((_, i) => (1 - fade) * lines[i] + fade * dead[i])
-  return [...rgb, 255]
+  let fade = pow(MIN_SCALE / scale, FADE_EXPONENT)
+  let [r, g, b] = [...Array(3)].map((_, i) => (1 - fade) * lines[i] + fade * dead[i])
+  return `rgba(${r},${g},${b},255)`
 }
 
 let {ceil, pow} = Math
